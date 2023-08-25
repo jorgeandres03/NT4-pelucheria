a97df9484dbf04049a039cf31d65c0b21625a83c
dayana <dayismo12345@gmail.com>
2023-08-25 11:16:27 -0500

    NT4 se crea inventario de peluches

---
+++ peluchitos.py
@@ -0,0 +1,24 @@
+Peluches=["Richi","Conejito","Aguacatico"]
+
+print("Pelucheria PELUCHITO")
+print("........")
+print("1. Agregar productos a la bodega")
+print("2. Ver productos en bodega")
+print("3. Obtener valor del inventario")
+print("4. Ver productos más vendidos")
+print("5. Salir")
+opcion=0
+
+while opcion!=5:
+    opcion=int(input("Digita un numero"))
+    if opcion==1:
+        print("Usted esta en la opcion 1 ")
+    elif opcion == 2:
+        print("Usted esta en la opcion 2 ")
+    elif opcion == 3:
+        print("Usted esta en la opcion 3 ")
+    elif opcion == 4:
+        print("Usted esta en la opcion 4 ")
+    else:
+        print("Opcion invalida") 
+print("Sali del ciclo")
